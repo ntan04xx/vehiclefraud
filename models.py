e95aec61868b470b91c60dd858fa3558d0e5f20e
ntan04xx <nathantan04@gmail.com>
2024-09-04 22:42:24 +1000

    Removed irrelevant factors

--- models.py
+++ models.py
@@ -8,11 +8,11 @@ fraud_df = pd.read_csv('fraud.csv')
 fraud_encoded = pd.get_dummies(fraud_df, drop_first=True)
 
 target = fraud_encoded['FraudFound_P']
-cov = fraud_encoded.drop('FraudFound_P', axis = 1)
+cov = fraud_encoded[['Sex_Male', 'Age', 'AgeOfVehicle_3 years', 'AgeOfVehicle_4 years', 'AgeOfVehicle_5 years', 'AgeOfVehicle_6 years', 'AgeOfVehicle_7 years', 'AgeOfVehicle_more than 7', 'AgeOfVehicle_new', 'AccidentArea_Urban', 'DriverRating', 'WitnessPresent_Yes', 'Year']]
 cov_train, covd_test, target_train, target_test = train_test_split(cov, target, test_size=0.2, random_state=23)
 
 # Creating models
-model = LogisticRegression(max_iter=10000)
+model = LogisticRegression(max_iter=10000) # This model has highest accuracy of 94%
 model.fit(cov_train, target_train)
 
 fraud_pred = model.predict(covd_test)
